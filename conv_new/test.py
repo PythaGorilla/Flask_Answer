9fe5dbc88cdfb4394be3b993a0f108046acad05a
mojians <mojians@qq.com>
2015-10-03 01:55:32 -0400

    add testing case

--- conv_new/test.py
+++ conv_new/test.py
@@ -1,6 +1,6 @@
 __author__ = 'Jian Mo'
 import os
-import client.client_main as flaskr
+import client.client_main as client_main
 import unittest
 import client.post_client as cl
 from scrapy.selector import Selector
@@ -8,9 +8,10 @@ from scrapy.selector import Selector
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        # self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        # flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
+
+        # import web client from flask module
+
+        self.app = client_main.app.test_client()
         self.uid="1"
         self.cid="1"
         self.app.post('/',data=dict(uid=self.uid,cid=self.cid))
@@ -29,13 +30,15 @@ class FlaskrTestCase(unittest.TestCase):
    # trying to recursively traverse all questions
     def test_questions(self):
 
-        # if it's initial question
+        # if it's initial question,then post
 
         if self.qid=="101":
             rv=self.app.post('/main',buffered=True, content_type='multipart/form-data', data={"uid":self.uid,"qid":self.qid,"cid":self.cid,"Choices":"1"},follow_redirects=True)
             print "rv.data=",rv.data
-            #choices=cl.JsonParser(rv.data["choices"])
             sel = Selector(rv)
+
+            #read possible choices from web page
+
             self.choices = sel.xpath(
                 '//select//option//@value').extract()
             self.qid=sel.xpath(
@@ -50,10 +53,12 @@ class FlaskrTestCase(unittest.TestCase):
 
             # otherwise looping the posting answer process
 
+            # post every possible answer to server and test if feedback ok
             for count,choice in enumerate(self.choices,start=0):
 
                 #recursion call
                 self.test_questions()
+
                 rv=self.app.post('/main', buffered=True, content_type='multipart/form-data', data={"uid":self.uid,"qid":self.qid[count],"cid":self.cid,"Choices":choice}, follow_redirects=True,)
 
                 print "choices=",rv.data
@@ -71,7 +76,7 @@ class FlaskrTestCase(unittest.TestCase):
                 else:
                     self.choices=choices
                     self.qid=qid
-                    #self.test_questions()
-        
+                    self.test_questions()
+
 if __name__ == '__main__':
     unittest.main()
